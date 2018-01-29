e572404061f9a0ff4b789a7e2328b5aaaef80dfa
Alioune <alioune.mane@qualshore.com>
2018-01-29 16:33:22 +0000

    commit du 29 janvier 2018 a 16h 33

---
+++ src/app/liste-evenements/liste-evenements.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared_service/user.service';
+import { Http, Headers,Response,RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-liste-evenements',
+  templateUrl: './liste-evenements.component.html',
+  styleUrls: ['./liste-evenements.component.css']
+})
+export class ListeEvenementsComponent implements OnInit {
+
+
+  EventByUser: any;
+  urls: any;
+  statusCode: number;
+
+
+  constructor(private userService : UserService, public http: Http, private router : Router) { }
+
+  ngOnInit() {
+    this.getEventByUser();
+  }
+
+  getEventByUser() {
+
+    this.userService.getEventByUser().subscribe(
+      data => {
+        this.EventByUser = data.events;
+        this.urls = data.urls;
+
+        console.log(this.EventByUser);
+        },
+      errorCode => {
+        this.statusCode = errorCode
+        }
+    );
+  } 
+
+}
