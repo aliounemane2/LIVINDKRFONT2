41e8a07f300995e972d8a36f351309b1cf226622
roussiba <roussibadiakhate@hotmail.fr>
2018-02-06 13:09:35 +0000

    update password gestion profil

--- src/app/Component/sidebar/sidebar.component.ts
+++ src/app/Component/sidebar/sidebar.component.ts
@@ -4,10 +4,10 @@ import { ProfilService } from './../../service/profil.service';
 import { CustomOption } from './../../service/CustomOption';
 import { TokenService } from './../../service/token.service';
 import { user } from './../../login/user';
-import { Component, OnInit, ViewContainerRef} from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { ToastsManager } from 'ng2-toastr/src/toast-manager';
 import { RedirectService } from '../../service/redirect.service';
-import { trigger, state, style, animate,transition, keyframes} from '@angular/animations';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 import * as $ from 'jquery';
 import { RegisterService } from '../../service/register.service';
 
@@ -15,8 +15,8 @@ import { RegisterService } from '../../service/register.service';
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  providers: [HeaderComponent,DashboardComponent],
-  animations : [
+  providers: [HeaderComponent, DashboardComponent],
+  animations: [
     trigger('heroState', [
       state('inactive', style({
         backgroundColor: '#eee',
@@ -24,153 +24,190 @@ import { RegisterService } from '../../service/register.service';
         left: '-415px',
         height: '0px'
       })),
-      state('active',   style({
+      state('active', style({
         backgroundColor: '#cfd8dc',
         transform: 'scale(1.1)',
         left: '0',
-        height:'60px'
+        height: '60px'
       })),
       transition('inactive => active', animate('1000ms ease-in', keyframes([
-        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-        style({opacity: 1, transform: 'translateY(35px)',  offset: 0.5}),
-        style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
+        style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+        style({ opacity: 1, transform: 'translateY(35px)', offset: 0.5 }),
+        style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 })
       ]))),
-      transition('active => inactive', animate('1000ms ease-out',keyframes([
-        style({opacity: 0, transform: 'translateY(-55%)', offset: 0}),
-        style({opacity: 1, transform: 'translateY(45px)',  offset: 0.5}),
-        style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
+      transition('active => inactive', animate('1000ms ease-out', keyframes([
+        style({ opacity: 0, transform: 'translateY(-55%)', offset: 0 }),
+        style({ opacity: 1, transform: 'translateY(45px)', offset: 0.5 }),
+        style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 })
       ])))
     ])
   ]
 })
 export class SidebarComponent implements OnInit {
 
-  password : string;
-  passwordConfirm : string;
-  utilisateur : user = new user();
+  password: string;
+  passwordConfirm: string;
+  oldpassword : string;
+  utilisateur: user = new user();
   utilisateurTest: user = new user();
-  profilOk : boolean;
+  profilOk: boolean;
   message: string;
-  state:string = 'inactive';
-  verifpassword :boolean;
+  state: string = 'inactive';
+  verifpassword: boolean;
+  nom: string;
+  prenom: string;
+  telephone: string;
+  dateNaissance: string;
 
-  constructor(private service: RegisterService, private dashboard: DashboardComponent, private cheader : HeaderComponent, private redirect: RedirectService, private tokenservice: TokenService, private toastr: ToastsManager,vcr: ViewContainerRef, private profil: ProfilService) {
+  constructor(private service: RegisterService, private dashboard: DashboardComponent, private cheader: HeaderComponent, private redirect: RedirectService, private tokenservice: TokenService, private toastr: ToastsManager, vcr: ViewContainerRef, private profil: ProfilService) {
     this.toastr.setRootViewContainerRef(vcr);
-   }
+  }
 
-   toggleState() {
+  toggleState() {
     this.state = this.state === 'active' ? 'inactive' : 'active';
   }
 
   ngOnInit() {
-    this.verifpassword= true;
+    this.verifpassword = true;
     this.profilOk = true;
-    if(this.tokenservice.isAuthorized() === false){
+    if (this.tokenservice.isAuthorized() === false) {
       this.redirect.redirectTologinForParam("Veuillez vous connecter pour accéder aux ressources de l'application");
-    }else{
-      let pseudo:string = this.tokenservice.getPseudo();
+    } else {
+      let pseudo: string = this.tokenservice.getPseudo();
       this.service.getUtilisateur(pseudo).subscribe(
         data => {
           this.utilisateur = data["user"];
           this.tokenservice.setUtilisateur(this.utilisateur);
           this.utilisateurTest = JSON.parse(this.tokenservice.getUtilisateur());
-
+          this.nom = this.utilisateur.nom;
+          this.prenom = this.utilisateur.prenom;
+          this.dateNaissance = this.utilisateur.dateNaissance;
+          this.telephone = this.utilisateur.telephone;
           this.tokenservice.removeUtilisateur();
           this.ChangeDetailUser(this.utilisateur.nom, this.utilisateur.prenom);
         },
-        errors =>{
+        errors => {
           console.log(errors);
         }
       );
-      
+
     }
-    
+
   }
 
-  setPhotoProfil(){
-    $(document).ready(function(){
+  setPhotoProfil() {
+    $(document).ready(function () {
       $('.fileinput-preview').prepend('<img class="img img-responsive" src="assets/img/avatar_defaut.png" />');
     });
   }
 
-  UpdatePassword(){
-    if(this.verifpassword == false){
-      alert("Les mots de passe sont différents.");
-    }else{
-      alert("ok");
-    }
-  }
-
-  UpdateEmail(){
+  UpdatePassword() {
     this.profilOk = false;
-    this.service.UpdatePassword(this.utilisateur.email,this.password,0).subscribe(
+    alert(this.verifpassword);
+    alert(this.password);
+    alert(this.passwordConfirm);
+
+    if (this.verifpassword == false) {
+      alert("Les mots de passe sont différents.");
+      this.profilOk = true;
+    } else {
+      this.service.UpdatePassword(this.utilisateurTest.email,this.password,0,this.oldpassword).subscribe(
         data => {
             if(data["corps"] === "0"){
                 this.toastr.success('Votre email est incorrecte !', 'Information!', CustomOption);
             }else{
-                this.message = data["message"];
+              if(data["corps"] == "1"){
+                this.tokenservice.removeToken();
+                this.tokenservice.removeUtilisateur();
+                this.tokenservice.removePseudo();
+                location.href = "/login/"+data["message"];
+                //this.redirect.redirectTologinForParam();
+              }
+              this.toastr.success(data["corps"]+" "+data["message"], 'Information!', CustomOption);
             }
+            console.log(data);
             this.profilOk = true;
         },
         error => {
             this.toastr.error('Serveur non accéssible. Veuillez reesayer.', 'Erreur!',CustomOption);
             this.profilOk = true;
         });
+    }
   }
 
-  UpdateUser(){
-    if(this.utilisateur.nom === this.utilisateurTest.nom && this.utilisateur.prenom === this.utilisateurTest.prenom
-    && this.utilisateur.telephone === this.utilisateurTest.telephone && this.utilisateur.dateNaissance === this.utilisateurTest.dateNaissance){
-      this.profilOk = false;
-      setTimeout(()=>{
+  UpdateEmail() {/*
+    this.profilOk = false;
+    this.service.UpdatePassword(this.utilisateur.email, this.password, 0).subscribe(
+      data => {
+        if (data["corps"] === "0") {
+          this.toastr.success('Votre email est incorrecte !', 'Information!', CustomOption);
+        } else {
+          this.message = data["message"];
+        }
         this.profilOk = true;
-      },500);
+      },
+      error => {
+        this.toastr.error('Serveur non accéssible. Veuillez reesayer.', 'Erreur!', CustomOption);
+        this.profilOk = true;
+      });*/
+  }
+
+  UpdateUser() {
+    if (this.utilisateur.nom === this.utilisateurTest.nom && this.utilisateur.prenom === this.utilisateurTest.prenom
+      && this.utilisateur.telephone === this.utilisateurTest.telephone && this.utilisateur.dateNaissance === this.utilisateurTest.dateNaissance) {
+      this.profilOk = false;
+      setTimeout(() => {
+        this.profilOk = true;
+      }, 500);
       this.messageToggle("Aucune Modification a étè détecté sur vos données personnelles");
-    }else{
+    } else {
       this.profilOk = false;
       this.profil.ModifierUtilisateur(this.utilisateur).subscribe(
-      data => {
-        let result = JSON.parse(data["_body"]);
-        if(result.status === 0){
-          this.profilOk = true;     
-          this.messageToggle("Vos données personnées ont étè modifiées");
-          this.tokenservice.setUtilisateur(result.user);
-          this.utilisateurTest = result.user;
-          /*let dashboard = new DashboardComponent(this.tokenservice, this.redirect);
-          dashboard.ngOnInit();*/
-          //this.cheader.utilisateur = result.user;
-          this.ChangeDetailUser(result.user.nom, result.user.prenom);
-          //this.dashboard.ngOnInit();
-        }else{
-          this.messageToggle("Nous avons rencontre un probléme de modification de vos données personnelles. Veuillez reessayer !");
-        }      
-      },
-      erreur => {
-        this.profilOk = true;                
-        this.toastr.success("Erreur", "Information !", CustomOption);   
-      });
+        data => {
+          let result = JSON.parse(data["_body"]);
+          if (result.status === 0) {
+            this.profilOk = true;
+            this.messageToggle("Vos données personnées ont étè modifiées");
+            this.tokenservice.setUtilisateur(result.user);
+            this.utilisateurTest = JSON.parse(this.tokenservice.getUtilisateur());
+            this.tokenservice.removeUtilisateur();
+            this.ChangeDetailUser(result.user.nom, result.user.prenom);
+
+            /*let dashboard = new DashboardComponent(this.tokenservice, this.redirect);
+            dashboard.ngOnInit();*/
+            //this.cheader.utilisateur = result.user;
+            
+            //this.dashboard.ngOnInit();
+          } else {
+            this.messageToggle("Nous avons rencontre un probléme de modification de vos données personnelles. Veuillez reessayer !");
+          }
+        },
+        erreur => {
+          this.profilOk = true;
+          this.toastr.success("Erreur", "Information !", CustomOption);
+        });
+    }
   }
-    }
-    
-    messageToggle(message){
-      this.message = message
-        this.state = 'active';
-      setTimeout(()=>{
-        this.state = 'inactive';
-      },5000);
-    }
 
-    ChangeDetailUser(nom,prenom){
-      $(document).ready(function() {
-        $('.nom').html(" "+nom);
-        $('.prenom').html(" "+prenom);
-      });
-    }
+  messageToggle(message) {
+    this.message = message
+    this.state = 'active';
+    setTimeout(() => {
+      this.state = 'inactive';
+    }, 5000);
+  }
 
-    VerifierPassword(){
-      this.verifpassword = this.password !== this.passwordConfirm
-                          && this.passwordConfirm != undefined 
-                          && this.password != undefined ? false : true;
-    }
+  ChangeDetailUser(nom, prenom) {
+    $(document).ready(function () {
+      $('.nom').html(" " + nom);
+      $('.prenom').html(" " + prenom);
+    });
+  }
+
+  VerifierPassword() {
+    this.verifpassword = this.password !== this.passwordConfirm
+      && this.passwordConfirm != undefined
+      && this.password != undefined ? false : true;
+  }
 
 }
